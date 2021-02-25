9c3b92c159cb990029af1a56d8111e689d2147ff
Ziehe Enrico <E.Ziehe@strube.net>
2021-02-25 15:56:55 +0100

    so tut er jedoch mit meckerei

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 /*
  * The MySensors Arduino library handles the wireless radio link and protocol
  * between your home built sensors/actuators and HA controller of choice.
@@ -25,50 +24,43 @@
  *
  */
 
-#define MY_BAUD_RATE 115200
-
 // Enable debug
 #define MY_DEBUG
 
 // Enable OTA debugging to Node 0
-#define MY_DEBUG_OTA (201)
-
-// Allow sending logs without MY_DEBUG_OTA enabled
-// #define MY_OTA_LOG_SENDER_FEATURE
-
+// #define MY_DEBUG_OTA (0)
 // Enable OTA log display
-//#define MY_OTA_LOG_RECEIVER_FEATURE
+#define MY_OTA_LOG_RECEIVER_FEATURE
+// Allow sending logs without MY_DEBUG_OTA enabled
+//#define MY_OTA_LOG_SENDER_FEATURE
 
 // Disable echoing of debug messages
 //#define MY_DEBUG_OTA_DISABLE_ECHO
-#define MY_NODE_ID 200
+
+#define MY_NODE_ID 201
+
 // for NRF24DUINO you have to specify a different CE-Pin for NRF24L01+
 #define MY_RF24_CE_PIN 7
+
 // Enable and select radio type attached
 #define MY_RADIO_RF24
 //#define MY_RADIO_NRF5_ESB
 //#define MY_RADIO_RFM69
 //#define MY_RADIO_RFM95
 
-// Enable repeater functionality for this node
-#define MY_REPEATER_FEATURE
+// Set blinking period
+#define MY_DEFAULT_LED_BLINK_PERIOD 200
+
+// Inverses the behavior of leds
+#define MY_WITH_LEDS_BLINKING_INVERSE
+
+// Flash leds on rx/tx/err
+// Uncomment to override default HW configurations
+//#define MY_DEFAULT_ERR_LED_PIN 4  // Error led pin
+#define MY_DEFAULT_RX_LED_PIN  9  // Receive led pin
+//#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
 
 #include <MySensors.h>
-
-#define RELAY_PIN 4  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-#define NUMBER_OF_RELAYS 1 // Total number of attached relays
-#define RELAY_ON 1  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
-
-void before()
-{
-	for (int sensor=1, pin=RELAY_PIN; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
-		// Then set relay pins in output mode
-		pinMode(pin, OUTPUT);
-		// Set relay to last known state (using eeprom storage)
-		digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
-	}
-}
 
 void setup()
 {
@@ -77,29 +69,24 @@ void setup()
 void presentation()
 {
 	// Send the sketch version information to the gateway and Controller
-	sendSketchInfo("Relay", "1.0");
-
-	for (int sensor=1, pin=RELAY_PIN; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
-		// Register all sensors to gw (they will be created as child devices)
-		present(sensor, S_BINARY);
-	}
+	sendSketchInfo("LoggerSensor", "1.0");
 }
 
+// Arduino loop
+//int c=0;
 void loop()
 {
+	// // Wait some time
+	// if (sleep(3000)==MY_SLEEP_NOT_POSSIBLE) {
+	// 	delay(3000);
+	// }
 
-}
+	// // Count loops
+	// c++;
 
-void receive(const MyMessage &message)
-{
-	// We only expect one type of message from controller. But we better check anyway.
-	if (message.getType()==V_STATUS) {
-		// Change relay state
-		digitalWrite(message.getSensor()-1+RELAY_PIN, message.getBool()?RELAY_ON:RELAY_OFF);
-		// Store state in eeprom
-		saveState(message.getSensor(), message.getBool());
-		// Write some debug info
+	// // A debug message
+	// DEBUG_OUTPUT(PSTR("DEBUG\nc=%" PRId16 "\nmillis=%" PRId32 "\n"), c, hwMillis());
 
-		DEBUG_OUTPUT(PSTR("Incoming change for sensor: % " PRId16 ", New status: %" PRId16 "\n"), message.getSensor(), message.getInt());
-	}
+	// // Send a log message to a node, requesting that the message is echoed back to this node
+	// OTALog(0, true, PSTR("LOG\nc=%" PRId16 "\nmillis=%" PRId32 "\n"), c, hwMillis());
 }
